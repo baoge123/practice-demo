35fb81d8008a61b4977ac26495fd33cb71aff036
baogerr <1321690683@qq.com>
2021-09-09 09:38:27 +0800

    map

---
+++ Map.js
@@ -0,0 +1,14 @@
+// 多条件判断
+let actions = new Map([
+    [1, ["3333","000000"]],
+    [2, ['fail', 'FailPage']],
+    [3, ['fail', 'FailPage']],
+    [4, ['success', 'SuccessPage']],
+    [5, ['cancel', 'CancelPage']],
+    ['default', ['other', 'Index']],
+  ])
+  console.log(actions)
+    function fun  (state) {
+     let arr = actions.get(state) || actions.get('default')
+     console.log(arr)
+  }
